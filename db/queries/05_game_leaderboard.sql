c8bce6998e1ae8eafb8574f8996ec039ce83597e
Owen Ripley <ojripley@gmail.com>
2019-10-18 17:02:52 -0400

    add game_leaderboard query

---
+++ db/queries/05_game_leaderboard.sql
@@ -0,0 +1,8 @@
+SELECT users.username, sum(results.score) as users_total_game_score
+FROM users
+JOIN results ON user_id = users.id
+JOIN matches ON match_id = matches.id
+WHERE matches.gametype = 'goofspiel'
+GROUP BY users.username
+ORDER BY users_total_game_score desc
+LIMIT 10;
