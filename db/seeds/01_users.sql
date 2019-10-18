327d227101ca61a52aa63399e0f47de0d73b5123
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-18 20:59:39 +0000

    add query for single match history and added seed data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -5,11 +5,19 @@ VALUES ('Owen', 'password', 'http://www.messagescollection.com/wp-content/upload
 
 INSERT INTO matches (gametype)
 VALUES ('goofspiel'),
-('goofspiel');
+('goofspiel'),('war'),('war'),('goofspiel'),('war');
 
 INSERT INTO results (match_id, user_id, score, finished_position)
 VALUES (1, 1, 20, 2),
 (1, 2, 30, 1),
-(2, 1, 20, 2),
-(2, 2, 30, 1);
+(2, 1, 90, 1),
+(2, 2, 20, 2);
+(3, 1, 90, 1),
+(3, 2, 20, 2);
+(4, 1, 90, 1),
+(4, 2, 20, 2);
+(5, 1, 90, 1),
+(5, 2, 20, 2);
+(6, 1, 90, 1),
+(6, 2, 20, 2);
 
