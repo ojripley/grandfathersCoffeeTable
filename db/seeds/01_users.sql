d4f3189a78f14990ddfd0371b3ceab0d5308c9d1
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-18 20:20:04 +0000

    Fix typos

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,12 +3,13 @@ INSERT INTO users (username, password, profile_picture)
 VALUES ('Owen', 'password', 'http://www.messagescollection.com/wp-content/uploads/2015/04/cute-fb-profile-picture.jpg'),
 ('Thilakshan', 'password', 'http://www.messagescollection.com/wp-content/uploads/2015/04/cute-fb-profile-picture.jpg');
 
+INSERT INTO matches (gametype)
+VALUES ('goofspiel'),
+('goofspiel');
+
 INSERT INTO results (match_id, user_id, score, finished_position)
 VALUES (1, 1, 20, 2),
 (1, 2, 30, 1),
 (2, 1, 20, 2),
 (2, 2, 30, 1);
 
-INSERT INTO matches (gametype)
-VALUES ('goofspiel'),
-('goofspiel');
