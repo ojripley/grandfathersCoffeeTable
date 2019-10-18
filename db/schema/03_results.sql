1ca2e923dd5d78eaaf75738dbc284b2e158d9e4f
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-18 19:51:44 +0000

    Add schema based

---
+++ db/schema/03_results.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate results table (Example)
+
+DROP TABLE IF EXISTS results CASCADE;
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  match_id INTEGER REFERENCES matches(id),
+  score INTEGER NOT NULL,
+  finished_position INTEGER NOT NULL;
+);
