dd797bcd7b30bf0d26826d02c7a647226ce32cc5
Owen Ripley <ojripley@gmail.com>
2019-10-18 17:06:08 -0400

    fix seed

--- db/queries/05_game_leaderboard.sql
+++ db/queries/05_game_leaderboard.sql
@@ -2,7 +2,7 @@ SELECT users.username, sum(results.score) as users_total_game_score
 FROM users
 JOIN results ON user_id = users.id
 JOIN matches ON match_id = matches.id
-WHERE matches.gametype = 'goofspiel'
+WHERE matches.gametype = 'war'
 GROUP BY users.username
 ORDER BY users_total_game_score desc
 LIMIT 10;
