05e4465608bec768e76eed17dcc290adcc76b5c2
Owen Ripley <ojripley@gmail.com>
2019-10-20 16:23:09 -0400

    wrote mock test game for functionality testing

--- server/objects/managers.js
+++ server/objects/managers.js
@@ -104,53 +104,48 @@ class Goofspiel extends Game {
 
 
 
-
-// testing code
-
-// const p1 = new Player(1, 'name1');
-// const p2 = new Player(2, 'name2');
+//////////////////////////
+// mock game test code! //
+//////////////////////////
 
 
-// activeGames.addGame('goofspiel');
+// players join server
+const p1 = new Player(1, 'name1');
+const p2 = new Player(2, 'name2');
+activePlayers.addPlayer(p1);
+activePlayers.addPlayer(p2);
 
-// console.log(activeGames);
-// activeGames.removeGame('1');
-// console.log(activeGames);
+// new game created
+activeGames.addGame('goofspiel');
 
+// players join game (will need to write a function for this)
+activeGames['1'].players.push(p1);
+activeGames['1'].players.push(p2);
 
+// game deals deck out to players
+activeGames['1'].deal();
 
+// player 1 plays a card
+// the card is moved from their hand to the table, and the move is logged into the pendingMoves array
+// player 2 does the same thing
+activeGames['1'].pendingMoves.push(activeGames['1'].players[0].playCard(activeGames['1'].players[0].hand.cards[0], activeGames['1'].table.cards));
+activeGames['1'].pendingMoves.push(activeGames['1'].players[1].playCard(activeGames['1'].players[0].hand.cards[0], activeGames['1'].table.cards));
 
-// activePlayers.addPlayer(p1);
-// console.log(activePlayers);
-// activePlayers.removePlayer(p1);
-// console.log(activePlayers);
+// the pending moves are then evaluated (need to write logic for nextState method)
+// activeGames['1'].nextState(pendingMoves);
 
+// moves are then pushed from pending to history
+activeGames['1'].pushPendingToHistory();
 
-// const game = new Goofspiel(fullDeck);
-// game.deal();
+// REPEAT UNTIL GAME IS OVER
+// isGameDone();
 
+// when game is finished, remove from activeGames
+activeGames.removeGame(activeGames['1']);
 
-// game.pendingMoves.push(p1.playCard(p1.hand.cards[0], game.table.cards));
+// when players decide they are finished, they log out
+activePlayers.removePlayer(p1);
+activePlayers.removePlayer(p2);
 
-// console.log(`pending moves: ${game.pendingMoves[0].player.username}`);
-
-// game.pushPendingToHistory();
-
-// console.log(`pending moves: ${game.pendingMoves}`);
-// console.log(`past moves: ${game.moveHistory[0].player.username}`);
-
-
-
-// console.log(`player hand: ${p1.hand.cards[0].value}`);
-// console.log(`\n\n\n table: ${ game.table.cards[0].value }`);
-
-// console.log('player one hand');
-// console.log(p1.hand.cards);
-// console.log('\n\n\nplayer two hand');
-// console.log(p2.hand.cards);
-
-// console.log(`deck ${game.deck.cards.length}`);
-
-
-
-
+console.log(activePlayers);
+console.log(activeGames);
