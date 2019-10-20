e81c9ee89bfd1eafa70fb5b59c9b919b7ea4415f
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-20 16:38:39 +0000

    Add more navbar options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,14 @@
 $(() => {
-  $("#navbar").sld
-
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  /*
+    $.ajax({
+      method: "GET",
+      url: "/api/users"
+    }).done((users) => {
+      for (user of users) {
+        $("<div>").text(user.name).appendTo($("body"));
+      }
+    });;
+    */
 
 
 });
