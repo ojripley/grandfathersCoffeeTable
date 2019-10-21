20e3f42a42355e16fcd87a50f0184e942071bd94
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-21 00:54:58 +0000

    Add layout update (change size of player board, add opponent area that shrinks on mobile view. Add listener to cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,30 @@
 $(() => {
 
+
   //Nav bar logic:
   $('#menu-bars').on('click', (event) => {
 
     $('#navbar').toggleClass('hidden', 500);
+  });
+
+
+  $(function() {
+    $("#2C").draggable();
+  });
+
+
+  //Create some on-clicks for testing purposes:
+  console.log($(".playerHand"));
+  console.log('hi');
+
+  $('.card').on('click', (event) => {
+    // let $chosenCard = $(event.target);
+    let cardName = event.target.id;
+    console.log(cardName);
+    // console.log($chosenCard.attr("id"));
+    console.log('click');
+    //Makes a move if it is the player's turn
+
   });
 
   /*
