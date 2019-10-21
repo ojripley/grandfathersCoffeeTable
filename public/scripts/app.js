714ba55ff35338ea9ff83f57932aca40813db81b
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-21 23:27:32 +0000

    Add client side socket emits/on

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+const extractUserName = function(str) {
+
+  return str.split("=")[0];
+
+};
+
 $(() => {
-
-
+  const myUsername = document.cookie;
+  console.log(document.cookie);
   //Nav bar logic:
   $('#menu-bars').on('click', (event) => {
 
@@ -13,30 +19,120 @@ $(() => {
   });
 
 
-  //Create some on-clicks for testing purposes:
-  console.log($(".playerHand"));
-  console.log('hi');
+  // $('.card').on('click', (event) => {
+  //   // let $chosenCard = $(event.target);
+  //   let cardName = event.target.id;
+  //   console.log(cardName);
+  //   // console.log($chosenCard.attr("id"));
+  //   console.log('click');
+  //   //Makes a move if it is the player's turn
 
-  $('.card').on('click', (event) => {
-    // let $chosenCard = $(event.target);
-    let cardName = event.target.id;
-    console.log(cardName);
-    // console.log($chosenCard.attr("id"));
+  // });
+
+
+  let socket = io.connect('172.46.3.253:8080');
+
+  $('#high-scores').on('click', (event) => {
+    console.log("requesting the high scores");
+    socket.emit('requestLeaderBoard', { gametype: 'goofspiel' });
+  });
+
+  $("#goo12").on('click', (event) => {
     console.log('click');
-    //Makes a move if it is the player's turn
+    socket.emit('requestGame', { gametype: 'goofspiel', username: myUsername });
+  });
+
+
+  $("#war12").on('click', (event) => {
+    console.log('click');
+    socket.emit('requestGame', { gametype: 'war', username: myUsername });
+  });
+
+  $("#sev12").on('click', (event) => {
+    console.log('click');
+
+    socket.emit('requestGame', { gametype: 'sevens', username: myUsername });
+  });
+
+
+  let player;
+  socket.on('gameView', (data) => {
+    console.log("RENDER THIS:");
+
+    console.log(data); //Store the view based on the data provided
+    console.log('Player:')
+    console.log(data.player); //Array of players
+    player = data.player[0];
+    console.log('Table');
+    console.log(data.table);
+    console.log('opponents');
+    console.log(data.opponents);
+    console.log('Table');
+    console.log(data.table);
+    console.log('Gameid');
+    console.log(data.gameid);
+    console.log('Current Player id');
+    console.log(data.curPlayerId);
+  });
+
+  socket.on('newGame', (data) => {
+    console.log("ADD THIS NEW GAME");
+    console.log(data);
 
   });
 
-  /*
-    $.ajax({
-      method: "GET",
-      url: "/api/users"
-    }).done((users) => {
-      for (user of users) {
-        $("<div>").text(user.name).appendTo($("body"));
-      }
-    });;
+  socket.on('playerJoin', (data) => {
+    console.log("PLAYER HAS JOINED");
+    console.log(data);
+  });
+
+  socket.on('leaderBoard', (data) => {
+    console.log("Here is the leaderboard");
+    console.log(data);
+  });
+
+  //When a card is clicked call this:
+  $("#AD").on('click', (event) => {
+    console.log('uh');
+    socket.emit('move', 'hello'); //Send the client's player object
+    /*
+{
+      Player: player,
+      Card: player.hand.cards[0]
+    }
+
     */
+  });
+
+  socket.emit('msg', "hi there");
+  socket.on('endGame', (data) => {
+    console.log('This is the end of game data (leaderboard)');
+    console.log(data);
+  });
+
+  // socket.on('requestHistory'){
+
+  // }
+  //onMove send game id
+
+
+
+
+  socket.on('msg', (data) => {
+    console.log(data);
+  });
+  socket.on('connection', (data) => {
+    console.log(data);
+  })
+
+  socket.on('newGame', (data) => {
+    console.log(data);
+  });
+
+
 
 
 });
+
+
+
