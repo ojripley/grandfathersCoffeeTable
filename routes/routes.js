545be578d6c5417f6a789b55a2411cb2b24eb20c
Owen Ripley <ojripley@gmail.com>
2019-10-21 12:55:55 -0400

    restructure index.html for routing

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,10 @@
 // all express get() and post() routes go here
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = (db) => {
+
+
+  return router;
+};
--- server/server.js
+++ server/server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("../routes/users");
 const widgetsRoutes = require("../routes/widgets");
+const routes = require('../routes/routes');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('/', routes(db));
 // Note: mount other resources here, using the same pattern above
 
 
