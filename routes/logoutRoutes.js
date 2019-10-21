e307f029645ab0b3d1c6c9a3dd5f60797cffe489
Owen Ripley <ojripley@gmail.com>
2019-10-21 16:31:22 -0400

    add logout functionality

--- routes/logoutRoutes.js
+++ routes/logoutRoutes.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
+module.exports = () => {
+  router.post('/logout', (req, res) => {
+    // delete session cookie by setting it to null
+    req.session.username = null;
+    res.render('login', {loginAttempt: true});
+  });
 
-router.post('/logout', (req, res) => {
-  // delete session cookie by setting it to null
-  req.session.username = null;
-  res.render('/login');
-});
+  return router;
+};
--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ const usersRoutes = require("../routes/users");
 const widgetsRoutes = require("../routes/widgets");
 const loginRoutes = require('../routes/loginRoutes');
 const registerRoutes = require('../routes/registerRoutes');
+const logoutRoutes = require('../routes/logoutRoutes');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -51,6 +52,7 @@ app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use('/', loginRoutes(db));
 app.use('/', registerRoutes(db));
+app.use('/', logoutRoutes());
 // Note: mount other resources here, using the same pattern above
 
 
