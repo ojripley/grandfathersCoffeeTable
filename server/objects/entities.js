e59756eaf219879548d19bdc3fc1f096c90f7497
Owen Ripley <ojripley@gmail.com>
2019-10-22 21:25:39 -0400

    implment hashed passwords. move goofspiel game object to its own file

--- server/objects/entities.js
+++ server/objects/entities.js
@@ -22,15 +22,7 @@ class Stack {
   }
 
   moveCard(card, destination) {
-    // console.log(card);
-    // console.log(this.cards.indexOf(card));
     destination.push(this.cards.splice(this.cards.indexOf(card), 1)[0]);
-    // destination.push(this.cards.splice(this.cards.filter((cardToRemove, index) => {
-    //   if (cardToRemove.name === card.name) {
-    //     console.log(index);
-    //     return index;
-    //   }
-    // }), 1)[0]);
   }
 
   selectRandom() {
@@ -100,138 +92,9 @@ class Game {
   }
 }
 
-class Goofspiel extends Game {
-
-  constructor(id, fullDeck) {
-
-    let tempFullDeck = fullDeck.slice(0);
-
-    super(id, tempFullDeck);
-    this.gameType = 'goofspiel';
-    this.table.faceUp = true;
-  }
-
-  deal() {
-    console.log('\n\ndealing.....');
-
-    console.log(this.deck);
-    console.log();
-
-    for (let i = 0; i < 13; i++) {
-      this.deck.moveCard(this.deck.cards[0], this.players[0].hand.cards);
-      this.deck.moveCard(this.deck.cards[12 - i], this.players[1].hand.cards);
-      if (this.players.length === 3) {
-        this.deck.moveCard(this.deck.cards[24 - (i * 2)], this.players[2].hand.cards);
-      } else {
-        this.deck.moveCard(this.deck.cards[24 - (i * 2)], this.burntDeck.cards);
-      }
-    }
-
-    // first card is placed on the table
-    // this kicks off the the game
-    this.deck.moveCard(this.deck.selectRandom(), this.table.cards);
-  }
-
-  score() {
-    const prize = this.table.cards[0];
-
-    console.log(`the prize is ${prize.value}\n\n`);
-
-    let highestBidValue = 0;
-    let highestBiddingMoves = [];
-
-    for (let move of this.pendingMoves) {
-
-      console.log(`the current evaluated move is ${move.card.value}\n\n`);
-
-      if (move.card.value > highestBidValue) {
-        highestBiddingMoves = [];
-        highestBiddingMoves.push(move);
-        highestBidValue = highestBiddingMoves[0].card.value;
-      } else if (move.card.value === highestBidValue) {
-        highestBiddingMoves.push(move);
-      }
-    }
-
-    console.log(`the highest bid is ${highestBidValue}\n\n`);
-
-    if (highestBiddingMoves.length === 1) {
-
-      const playerToRecievePoints = this.players.filter(player => highestBiddingMoves[0].player.username === player.username);
-
-      console.log(playerToRecievePoints);
-      // highestBiddingMoves[0].player.score += (highestBidValue + prize.value);
-      playerToRecievePoints[0].score += (highestBidValue + prize.value);
-    }
-
-
-    console.log('\nPLAYER SCORES:');
-    console.log(this.players[0].username + ': ' + this.players[0].score);
-    console.log(this.players[1].username + ': ' + this.players[1].score);
-    console.log();
-
-    for (let i = 0; i < this.players.length; i++) {
-      for (let j = 1; j < this.players.length; j++) {
-        if (this.players[j].score > this.players[i].score) {
-          let temp = this.players[j];
-          this.players[j] = this.players[i];
-          this.players[i] = temp;
-        }
-      }
-    }
-
-    for (let i = 0; i < this.players.length; i++) {
-      this.players[i].currentPosition = (i + 1);
-    }
-
-    // this.players.indexOf[this.players[i]]
-
-    this.table.cards.pop();
-
-  }
-
-  isValidMove(move) {
-    if (this.gameState === 'playing') {
-      for (let pendingMove of this.pendingMoves) {
-        if (move.player.username === pendingMove.player.username) {
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-
-  areAllMovesSubmitted() {
-    if (this.pendingMoves.length === this.players.length) {
-      return true;
-    }
-
-    return false;
-  }
-
-  isGameDone() {
-
-    console.log('\n\n\n\n' + this.gameState + '\n\n\n\n');
-
-    if (this.deck.cards.length === 0) {
-      this.gameState === 'finished';
-      console.log(`\n the winner is... ${this.players[0].username} \n\n`);
-      return true;
-    } else {
-
-      // proceed with the game
-      this.deck.moveCard(this.deck.selectRandom(), this.table.cards);
-
-      return false;
-    }
-  }
-}
-
 module.exports = {
   Player,
   Stack,
   Card,
   Move,
-  Goofspiel
 };
--- server/objects/managers.js
+++ server/objects/managers.js
@@ -1,4 +1,4 @@
-const { Player, Stack, Move, Goofspiel } = require('./entities');
+const { Goofspiel } = require('./goofspielGame');
 const fullDeck = require('./fullDeck');
 
 // constant objects (single instance entities only)
@@ -38,51 +38,3 @@ const activeGames = {
 };
 
 module.exports = { activePlayers, activeGames };
-
-
-
-//////////////////////////
-// mock game test code! //
-//////////////////////////
-
-
-// players join server
-// const p1 = new Player(1, 'name1');
-// const p2 = new Player(2, 'name2');
-// activePlayers.addPlayer(p1);
-// activePlayers.addPlayer(p2);
-
-// new game created
-// activeGames.addGame('goofspiel');
-
-// // players join game (will need to write a function for this)
-// activeGames['1'].players.push(p1);
-// activeGames['1'].players.push(p2);
-
-// // game deals deck out to players
-// activeGames['1'].deal();
-
-// // player 1 plays a card
-// // the card is moved from their hand to the table, and the move is logged into the pendingMoves array
-// // player 2 does the same thing
-// activeGames['1'].pendingMoves.push(activeGames['1'].players[0].playCard(activeGames['1'].players[0].hand.cards[0], activeGames['1'].table.cards));
-// activeGames['1'].pendingMoves.push(activeGames['1'].players[1].playCard(activeGames['1'].players[0].hand.cards[0], activeGames['1'].table.cards));
-
-// // the pending moves are then evaluated (need to write logic for nextState method)
-// // activeGames['1'].nextState(pendingMoves);
-
-// // moves are then pushed from pending to history
-// activeGames['1'].pushPendingToHistory();
-
-// // REPEAT UNTIL GAME IS OVER
-// // isGameDone();
-
-// // when game is finished, remove from activeGames
-// activeGames.removeGame(activeGames['1']);
-
-// // when players decide they are finished, they log out
-// activePlayers.removePlayer(p1);
-// activePlayers.removePlayer(p2);
-
-// console.log(activePlayers);
-// console.log(activeGames);
