a8844b690b977c3e0c1034506ae72ca2b945fc02
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-24 15:45:32 +0000

    modified how username is extracted

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,6 +1,10 @@
 //Gets username from cookie
 const extractUserName = function(str) {
   return str.split("=")[0];
+
+  //Use the version below once cookie is fixed:
+  return str.split(";").filter(match => match.indexOf('username') != -1)[0].split("=")[1];
+
 };
 
 //Returns true if the user is in the player array
